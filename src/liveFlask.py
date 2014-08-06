0f755c358a869ce77f98ed9270edebacfa0854fa
Yuri Stange <yuris@kth.se>
2014-08-06 02:47:30 +0200

    Initial upload

---
+++ src/liveFlask.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import subprocess
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+livestreamer = 0
+
+@app.route("/", methods = ['GET'])
+def hello():
+    return "Hello World"
+
+@app.route("/stream/play", methods = ['POST'])
+def start_stream():
+    global livestreamer
+
+    if(isinstance(livestreamer,subprocess.Popen)):
+        terminate_stream(livestreamer)
+
+    if not request.json or not 'url' in request.json:
+        return "Fel i url"
+
+    livestreamer = subprocess.Popen(["livestreamer", request.json['url'], "best", "-np", "omxplayer -o hdmi --win \"40 25 1880 1055\""])
+    return "Opening stream"
+
+@app.route("/stream/stop", methods = ['GET'])
+def stop_stream():
+    global livestreamer
+
+    if(isinstance(livestreamer,subprocess.Popen)):
+        terminate_stream(livestreamer)
+    
+    return "Stopping stream"
+
+
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    global livestreamer
+    if(isinstance(livestreamer,subprocess.Popen)):
+        terminate_stream(livestreamer)
+    shutdown_server()
+    return 'Server shutting down...'
+
+
+def terminate_stream(livestreamer):
+    if(livestreamer.poll() is None):
+        livestreamer.terminate()
+        livestreamer.wait()
+        return
+    else:
+        return
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=1337, debug=True)
